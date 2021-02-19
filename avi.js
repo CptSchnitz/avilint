87ef14a87a1ecef6c12cfe6050a788ad70ab6bae
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2021-02-19 18:06:47 +0200

    chore: first try 3.0

---
+++ avi.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-logi */
+import * as core from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+import * as glob from '@actions/glob';
+import {readFile} from 'fs/promises';
+
+    // eslint-disable-next-line @typescript-eslint/camelcase
+while (true) {
+  console.log('can you do a logi run?');
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 // /* eslint-disable no-undef */
 import * as core from '@actions/core';
-import { context, getOctokit } from '@actions/github';
+import {context, getOctokit} from '@actions/github';
 import * as glob from '@actions/glob';
+import {readFile} from 'fs/promises';
+import table from 'markdown-table';
 
 async function run(): Promise<void> {
   const pr = context.payload.pull_request;
@@ -15,9 +17,32 @@ async function run(): Promise<void> {
   // Get owner and repo from context
   const owner = context.repo.owner;
   const repo = context.repo.repo;
-  const pattern = '**/*.js';
-  const globber = await glob.create(pattern);
+  const patterns = core.getInput('GLOBS').split(',').join('\n');
+  const globber = await glob.create(patterns);
   const files = await globber.glob();
+  // for await (const file of globber.globGenerator()) {
+
+  // }
+  const regex = new RegExp(/^\/\* eslint-disable (?<avi>.*) \*\/$/, 'g');
+  const promises = files.map(async file => {
+    const content = (await readFile(file)).toString();
+    let array: RegExpExecArray | null;
+    const result: string[] = [];
+
+    while ((array = regex.exec(content))) {
+      if (!array || !array.groups) {
+        break;
+      }
+      result.push(array.groups['avi']);
+    }
+    return {file, rules: result};
+  });
+
+  const content = (await Promise.all(promises))
+    .filter(m => m.rules.length > 0)
+    .map(m => [m.file, m.rules.join(',')]);
+
+  const res = table([['file', 'rules'], ...content]);
 
   // Create a comment on PR
   // https://octokit.github.io/rest.js/#octokit-routes-issues-create-comment
@@ -26,10 +51,12 @@ async function run(): Promise<void> {
     repo,
     // eslint-disable-next-line @typescript-eslint/camelcase
     issue_number: pr.number,
-    body: files.join('\n')
+    body: res
   });
   core.debug(`created comment URL: ${response.data.html_url}`);
   core.debug('Wrote a comment successfully');
 }
 
-run().catch(() => {core.setFailed(':(')});
+run().catch(() => {
+  core.setFailed(':(');
+});
